0054311888e089c19fc30dd7300a7b28fa975e85
Evelyyyynnnn <vickydu1213@gmail.com>
2025-02-19 22:41:23 -0500

    init

--- blog.config.js
+++ blog.config.js
@@ -1,4 +1,4 @@
-123339517526692// 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/article/how-to-config-notion-next#c4768010ae7d44609b744e79e2f9959a
+//123339517526692// 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/article/how-to-config-notion-next#c4768010ae7d44609b744e79e2f9959a
 const BLOG = {
   // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
   NOTION_PAGE_ID:process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5',
